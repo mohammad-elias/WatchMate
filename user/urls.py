0faa82385d050d3d79fa960ae5c8632718fb3e5e
Mohammad Elias <90592770+mohammad-elias@users.noreply.github.com>
2024-05-01 09:18:06 +0600

    urls_update.py

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import UserRegistrationApiView,logout_view
+from .views import UserRegistrationApiView,logout_view,CustomObtainAuthToken
 
 
 urlpatterns = [
-    path('login/',obtain_auth_token,name='login'),
+    path('login/',CustomObtainAuthToken.as_view(),name='login'),
     path('register/',UserRegistrationApiView.as_view(),name='register'),
     path('logout/',logout_view,name='logout'),
 ]
