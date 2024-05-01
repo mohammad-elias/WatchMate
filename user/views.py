b2ccfca9a89f57c4bdd7575bafa0567112619127
Mohammad Elias <90592770+mohammad-elias@users.noreply.github.com>
2024-05-01 09:15:49 +0600

    login_update.py

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 # Create your views here.
@@ -29,6 +30,13 @@ class UserRegistrationApiView(APIView):
             data = serializer.errors
             
         return Response(data)
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'user_id': token.user_id})
             
 
 @api_view(['POST',])
